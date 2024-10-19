98c04290ef43ed70052c99f648070f0b562bc19b
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-10-19 14:20:49 +0900

    update

--- test/test_jtable.rb
+++ test/test_jtable.rb
@@ -10,13 +10,25 @@ class TestYS1JTable < Minitest::Test
 
   def test_add_data
     @jtable.add_data("a", "a")
-    @jtable.add_data("b", "b" * 2)
-    @jtable.add_data("c", "c" * 3)
-    assert_equal({ a: ["a"], b: ["bb"], c: ["ccc"] }, @jtable.data)
+    @jtable.add_data("b", "b")
+    @jtable.add_data("c", "c")
+    assert_equal({ a: ["a"], b: ["b"], c: ["c"] }, @jtable.data)
   end
 
-  def test_table_2_data
-    data = YS1::JTable.table_2_data("testdata/jtable/jtable.csv")
+  def test_cross_join
+    @jtable.add_data("a", "a-data")
+    @jtable.add_data("a", "a-data2")
+    @jtable.add_data("b", "b-data")
+    @jtable.add_data("b", "b-data2")
+    @jtable.add_data("c", "c-data")
+    cross_join = @jtable.cross_join
+
+    assert_equal(4, cross_join.length) # 2 x 2 x 1
+    assert(cross_join.include?(%w[a-data2 b-data2 c-data]))
+  end
+
+  def test_to_h
+    data = YS1::JTable.to_h("testdata/jtable/jtable.csv")
     assert_equal(Hash, data.class)
     assert_equal(2, data.length)
     assert_equal(%w[1 2], data.keys)
