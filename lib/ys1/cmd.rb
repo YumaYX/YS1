5867cd405b2fd77d6d6eddead9e0b49703206dfc
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-01-14 23:46:17 +0900

    Display usage instructions and exit the program.

--- lib/ys1/cmd.rb
+++ lib/ys1/cmd.rb
@@ -5,12 +5,15 @@ module YS1
   # Cmd module
   module Cmd
     class << self
-      # Display usage information and exit the program.
+      # Displays usage instructions and exits the program.
       #
-      # @param args [String] The usage information to display.
-      # @return [void]
-      def usage(commad, args)
-        warn "Usage:\n  #{File.basename(commad)} #{args}"
+      # @param command [String] The name of the command being executed.
+      # @param args [Array<String>] A list of arguments for the command. Defaults to an empty array.
+      # @return [void] This method does not return, as it exits the program.
+      def usage(command, *args)
+        message = "Usage:\n  #{File.basename(command)}"
+        message += " #{args.join(' ')}" unless args.empty?
+        warn message
         exit 1
       end
     end
