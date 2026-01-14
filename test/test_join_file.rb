c921054303f78970a29e8af52c569f5a204be3bb
YumaYX <86939129+YumaYX@users.noreply.github.com>
2026-01-14 19:02:58 +0900

    test_join_file.rb の作成

---
+++ test/test_join_file.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require_relative "helper"
+require "tempfile"
+
+class TestYS1Join < Minitest::Test
+  def with_tempfile(lines)
+    file = Tempfile.new("ys1_join")
+    file.write(lines.join("\n"))
+    file.close
+    yield(file.path)
+  ensure
+    file.unlink
+  end
+
+  def test_cross_files_with_block
+    with_tempfile(["A", "B"]) do |f1|
+      with_tempfile(["1", "2"]) do |f2|
+        result = []
+
+        YS1::Join.cross_files(f1, f2) do |a, b|
+          result << "#{a}#{b}"
+        end
+
+        assert_equal(
+          ["A1", "A2", "B1", "B2"],
+          result
+        )
+      end
+    end
+  end
+
+  def test_cross_files_returns_enumerator
+    with_tempfile(["X"]) do |f1|
+      with_tempfile(["Y", "Z"]) do |f2|
+        enum = YS1::Join.cross_files(f1, f2)
+
+        assert_instance_of(Enumerator, enum)
+        assert_equal(
+          ["XY", "XZ"],
+          enum.map { |a, b| "#{a}#{b}" }
+        )
+      end
+    end
+  end
+
+  def test_cross_files_with_empty_file
+    with_tempfile(["A"]) do |f1|
+      with_tempfile([]) do |f2|
+        result = []
+
+        YS1::Join.cross_files(f1, f2) do |values|
+          result << values
+        end
+
+        assert_equal([], result)
+      end
+    end
+  end
+end
