03c617b36a08e24cc1717aa161cf2d86e02d0ee2
YumaYX <86939129+YumaYX@users.noreply.github.com>
2026-01-15 18:44:51 +0900

    test_hash_to_class.rb の更新

--- test/test_hash_to_class.rb
+++ test/test_hash_to_class.rb
@@ -2,15 +2,15 @@
 
 require_relative "helper"
 
-class TestYS1HashToDynamicClass < Minitest::Test
+class TestYS1HashToClass < Minitest::Test
   # Set up a sample hash representing Ruby info
   def setup
     @ruby_info = { "version" => "4.0.0", "release_date" => "2025-12-25" }
   end
 
   # Test converting a hash to a dynamic class and checking attribute values
-  def test_to_dynamic_class
-    instance = @ruby_info.to_dynamic_class.new
+  def test_to_anon_class
+    instance = @ruby_info.to_anon_class.new
 
     @ruby_info.each do |key, value|
       assert_equal(value, instance.send(key))
@@ -19,7 +19,7 @@ class TestYS1HashToDynamicClass < Minitest::Test
 
   # Test defining an additional method on the dynamic class
   def test_extra
-    klass = @ruby_info.to_dynamic_class
+    klass = @ruby_info.to_anon_class
 
     klass.define_method(:new_method) do
       "ver. #{version}"
